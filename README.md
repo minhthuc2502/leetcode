c50f83b3be537da3b10a17aab3d494a8b42afe5c
PHAM Minh Thuc <46375464+minhthuc2502@users.noreply.github.com>
2020-03-22 19:03:21 +0100

    find indices of the two numbers such that they add up to a specific target

---
+++ README.md
@@ -0,0 +1,2 @@
+# Description
+This is all the problems that i have solved on leetcode to practice programming and learn more algorithme. 
---
+++ problem_solved_1.cpp
@@ -0,0 +1,24 @@
+/**
+ * Question easy 1
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+*/
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result(0);
+        for(int i = 0; i<nums.size();i++)
+        {
+            for (int j = i+1; j<nums.size(); j++)
+            {
+                if(nums[i]+nums[j] == target)
+                {
+                    result.push_back(i);
+                    result.push_back(j);
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+};
